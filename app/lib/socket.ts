542076d07325a4b8ae54b42540a08427675a1c3f
unknown <tudxtworkspace@gmail.com>
2026-03-20 01:05:03 +0700

    f5

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,6 +1,9 @@
 import { io as clientIO, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const SOCKET_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_SERVER_URL_PRODUCTION
+    : process.env.NEXT_PUBLIC_SERVER_URL_DEVELOPMENT;
 
 export const initSocket = (userId: string): Socket => {
   return clientIO(SOCKET_URL, {
