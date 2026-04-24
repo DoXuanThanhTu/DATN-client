6581b76e6fab653ee2c5f37d56714c41db64b854
unknown <tudxtworkspace@gmail.com>
2026-04-24 14:18:36 +0700

    aa

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -188,7 +188,7 @@ export default function ProductDetail() {
     );
 
   return (
-    <div className="max-w-6xl mx-auto p-4 bg-gray-50 min-h-screen">
+    <div className="w-full  md:w-4xl lg:w-6xl mx-auto p-4 bg-gray-50 min-h-screen">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {/* CHI TIẾT SẢN PHẨM */}
         <div className="lg:col-span-2 space-y-4">
